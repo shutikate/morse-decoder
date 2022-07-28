250d16b24fec87d9777353e4efdd6a851fbedf8b
Katsiaryna Shutsikava <shutikova.k@gmail.com>
2022-07-28 10:37:23 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,20 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+    const table = {};
+    Object.keys(MORSE_TABLE).forEach(function(key) {
+    table[key.split('').map((elem) => elem === '.' ? elem = '10' : elem = '11').join('').padStart(10, '0')] = MORSE_TABLE[key];
+    })
+
+    let arrayLetters = [];
+    while (expr !== '') {
+        arrayLetters.push(expr.slice(0, 10));
+        expr = expr.slice(10);
+    }
+
+    let result = arrayLetters.reduce(((acc, elem) => elem === '**********' ? acc += ' ' : acc += table[elem]), '');
+    return result;
 }
 
 module.exports = {
